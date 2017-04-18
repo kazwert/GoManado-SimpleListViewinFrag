892ab4ca6412fa71eae141b9a3f48fa065c632d8
kazwert <kazwert@gmail.com>
2017-04-18 15:20:25 +0700

    Initial commit

---
+++ app/src/main/java/com/kodekeras/kw/gomanado/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kodekeras.kw.gomanado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
